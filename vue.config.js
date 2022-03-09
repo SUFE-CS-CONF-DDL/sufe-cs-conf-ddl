253fd37f3aa9a95cd12f05f9e560e2e93520e914
SUFEHeisenberg <18817876918@163.com>
2022-03-09 23:53:30 +0800

    3.9 new commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'sufe-cs-conf-ddl',
+    }
+  }
+}
